5a33342eb39c2026513c92a501a954ac7ffca0b4
Dmytro Simanovskyi <dmytro_simanovskyi@epam.com>
2023-01-25 21:20:13 +0200

    Update titles and products description

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            Board Game Store
           </Link>
         </Typography>
 
